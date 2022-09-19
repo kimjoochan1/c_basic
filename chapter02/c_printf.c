ba35aa50d15d27e78a32282e8ec2372155171b8c
kimjoochan1 <112990652+kimjoochan1@users.noreply.github.com>
2022-09-19 10:48:32 +0900

    finished chapter01

---
+++ chapter02/c_printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// 제어문자
+// + \n : 한줄 개행
+// + \t : 탭(8칸 스페이스)
+int main(void)
+{
+    /* code */
+    printf("HELLO\n");
+    printf("Chulwoong \t Choi");
+
+    return 0;
+}
---
+++ chapter02/c_printf_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* code */
+    printf("나는 %d살 입니다.", 34);
+    return 0;
+}
